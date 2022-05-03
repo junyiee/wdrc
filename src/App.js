fef1e158f330908258b99678607436c5eeab8f16
Tilon <76069479+Kingtilon1@users.noreply.github.com>
2022-05-02 20:13:53 -0400

    sign in files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import Login from './pages/login';
+import Form from './Form';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
-};
+  return <Form />
+}
 
 export default App;
